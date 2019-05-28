9e2035597bb04c4989a5009fc3f92145f6752f6f
Domenic Murtari <domenic.murtari@gmail.com>
2019-05-28 09:07:54 -0600

    Create Module

--- apps/elements/src/app/app.module.ts
+++ apps/elements/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
+import { CardModule } from './card/card.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
+  imports: [BrowserModule, NxModule.forRoot(), CardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ apps/elements/src/app/card/card.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { CardComponent } from '../card/card.component';
+
+@NgModule({
+    declarations: [CardComponent],
+    entryComponents: [CardComponent]
+})
+export class CardModule {}
